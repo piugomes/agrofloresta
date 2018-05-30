f814347d15274f3b2652cfc849b14622c5b2bbc5
piugomes <pedroaugustog.pg20@gmail.com>
2018-05-30 01:38:11 -0300

    Versão 2.0 do Banco

--- agrofloresta.sql
+++ agrofloresta.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 09-Maio-2018 às 02:40
+-- Data de Criação: 30-Maio-2018 às 04:37
 -- Versão do servidor: 5.6.13
 -- versão do PHP: 5.4.17
 
@@ -29,24 +29,23 @@ USE `agrofloresta`;
 --
 
 CREATE TABLE IF NOT EXISTS `area` (
-  `ID_area` int(5) NOT NULL AUTO_INCREMENT,
-  `cod_localizacao` varchar(50) NOT NULL,
-  `nome` varchar(50) NOT NULL,
+  `ID_area` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_localizacao` int(100) NOT NULL,
+  `nome` varchar(100) NOT NULL,
   `uni_medida` varchar(10) NOT NULL,
-  `tamanho` double NOT NULL,
+  `tamanho` int(100) NOT NULL,
   PRIMARY KEY (`ID_area`),
-  KEY `cod_localizacao` (`cod_localizacao`),
-  KEY `ID_area` (`ID_area`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  KEY `cod_localizacao` (`cod_localizacao`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `area`
 --
 
 INSERT INTO `area` (`ID_area`, `cod_localizacao`, `nome`, `uni_medida`, `tamanho`) VALUES
-(1, '1', 'Monteiro', 'Hectare', 1000),
-(2, '1', 'Gomes', 'Alqueires', 1000),
-(3, '1', 'Jamil', 'Ares', 1000);
+(2, 2, 'Mata AtlÃ¢ntica', 'Alqueires', 500),
+(3, 3, 'Reserva ', 'Alqueires', 600),
+(4, 3, 'Mata', 'Alqueires', 600);
 
 -- --------------------------------------------------------
 
@@ -55,24 +54,25 @@ INSERT INTO `area` (`ID_area`, `cod_localizacao`, `nome`, `uni_medida`, `tamanho
 --
 
 CREATE TABLE IF NOT EXISTS `cultura` (
-  `ID_cultura` int(5) NOT NULL AUTO_INCREMENT,
-  `cod_area` int(5) NOT NULL,
-  `tipo` varchar(50) NOT NULL,
-  `nome_cul` varchar(50) NOT NULL,
+  `ID_cultura` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_area` int(100) NOT NULL,
+  `tipo` varchar(100) NOT NULL,
+  `nome_cul` varchar(100) NOT NULL,
   `renda` float NOT NULL,
   `gasto` float NOT NULL,
   `q_produzida` float NOT NULL,
   `q_esperada` float NOT NULL,
   PRIMARY KEY (`ID_cultura`),
   KEY `cod_area` (`cod_area`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `cultura`
 --
 
 INSERT INTO `cultura` (`ID_cultura`, `cod_area`, `tipo`, `nome_cul`, `renda`, `gasto`, `q_produzida`, `q_esperada`) VALUES
-(3, 1, 'Lenhosa', 'Pau-Brasil', 10000.5, 5000, 30000, 20750);
+(3, 2, 'alimento', 'cultura do milho', 100000, 4000, 5000, 5500),
+(4, 3, 'alimento', 'soja', 2300, 500, 4600, 4700);
 
 -- --------------------------------------------------------
 
@@ -81,14 +81,11 @@ INSERT INTO `cultura` (`ID_cultura`, `cod_area`, `tipo`, `nome_cul`, `renda`, `g
 --
 
 CREATE TABLE IF NOT EXISTS `localizacao` (
-  `ID_localizacao` int(5) NOT NULL AUTO_INCREMENT,
+  `ID_localizacao` int(100) NOT NULL AUTO_INCREMENT,
   `pais` varchar(50) NOT NULL,
   `estado` varchar(50) NOT NULL,
   `municipio` varchar(50) NOT NULL,
-  PRIMARY KEY (`ID_localizacao`),
-  KEY `ID_localizacao` (`ID_localizacao`),
-  KEY `ID_localizacao_2` (`ID_localizacao`),
-  KEY `ID_localizacao_3` (`ID_localizacao`)
+  PRIMARY KEY (`ID_localizacao`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
@@ -96,12 +93,18 @@ CREATE TABLE IF NOT EXISTS `localizacao` (
 --
 
 INSERT INTO `localizacao` (`ID_localizacao`, `pais`, `estado`, `municipio`) VALUES
-(1, 'Brasil', 'SÃ£o Paulo', 'Araraquara'),
-(2, 'Brasil', 'SÃ£o Paulo', 'Barretos');
+(2, 'Argentina', 'Rio de Janeiro', 'Rio de janeiro'),
+(3, 'Brasil', 'SÃ£o Paulo', 'Araraquara');
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Limitadores para a tabela `area`
+--
+ALTER TABLE `area`
+  ADD CONSTRAINT `area_ibfk_1` FOREIGN KEY (`cod_localizacao`) REFERENCES `localizacao` (`ID_localizacao`);
 
 --
 -- Limitadores para a tabela `cultura`
